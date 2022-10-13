d421d52f3f6038b561dc56cf48a8612aa25e3133
antoniocesarlima <antonio.pbucesar2005@gmail.com>
2022-10-13 08:27:53 -0300

    Reprodução do projeto Spring

---
+++ lab-padroes-projeto-spring-main/src/main/java/one/digitalinnovation/gof/Application.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Codigo reproduzido do desafio de projeto do Bootcamp Java Québec
+ * 
+ * @author antoniocesarlima
+ */
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
